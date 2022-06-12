5ac419016d17121937173af5dbe7164b2c14ce33
rydzwr <rydzwr@gmail.com>
2022-06-12 15:48:22 +0200

    Tasks 1-6 except 5 (improved)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,6 @@ public class Application
     public static void main(String[] args)
     {
         Kata kata = new Kata();
-        System.out.println(kata.add("//;\\n1;2"));
+        System.out.println(kata.add("//;\\n1;1002,1,,,,,,,,34,2,fghxxxx!@#$%^&*()xxxxxf1\\n2,3ghf//;\\n1;2gs,sdfg,-1"));
     }
 }
--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,29 +1,34 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Kata
 {
     int add(String numbers)
     {
+        ArrayList<String> strings = new ArrayList<>();
         int result = 0;
 
         if (numbers.length() == 0)
             return 0;
 
-        
+        numbers = numbers.replaceAll("[^1234567890]", "x");
 
-        numbers = numbers
-                .replace(",", "")
-                .replace("\n", "")
-                .replace("\r", "")
-                .replace("/", "")
-                .replace(";", "")
-                .replace(String.valueOf("\\n"), "");
+        String[] splited = numbers.split("x");
 
-        char[] chars = numbers.toCharArray();
+        strings.addAll(Arrays.asList(splited));
 
-        for (int i = 0; i < chars.length; i++)
-            result += Integer.parseInt(String.valueOf(chars[i]));
+        strings.removeAll(Arrays.asList("", "x"));
 
+        for (int i = 0; i < strings.size(); i++)
+        {
+            if (Integer.parseInt(strings.get(i)) > 1000)
+                strings.remove(i);
+
+            result += Integer.parseInt(String.valueOf(strings.get(i)));
+        }
 
         return result;
     }
