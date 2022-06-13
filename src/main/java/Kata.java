c60377e78635fe4c5f4957f9435ff8278ce592d9
rydzwr <rydzwr@gmail.com>
2022-06-13 21:12:48 +0200

    FIX: Much faster

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -8,33 +8,47 @@ public class Kata
 {
     int add(String numbers) throws NegativesNotAllowedException
     {
-        ArrayList<String> strings = new ArrayList<>();
         ArrayList<String> negatives = new ArrayList<>();
         int result = 0;
 
         if (numbers.length() == 0)
             return 0;
 
-        numbers = numbers.replaceAll("[^1234567890-]", "x");
+       /* numbers = numbers.replaceAll("[^1234567890-]", "x");
 
         String[] splited = numbers.split("x");
 
-        strings.addAll(Arrays.asList(splited));
-
-        strings.removeAll(Arrays.asList("", "x"));
-
-        for (int i = 0; i < strings.size(); i++)
+        for (String s : splited)
         {
-            if (Integer.parseInt(strings.get(i)) > 1000)
-                strings.remove(i);
+            int number = Integer.parseInt(s);
 
-            if (Integer.parseInt(strings.get(i)) < 0)
-            {
-                negatives.add(strings.get(i));
+            if (number < 0)
+                negatives.add(s);
+
+            if (number < 1000)
+                result += number;
+        }*/
+
+        numbers += "%";
+        char[] chars = numbers.toCharArray();
+        String numberStr = "";
+        for (char c : chars)
+        {
+            if (Character.isDigit(c) || c == '-')
+                numberStr += c;
+            else if (numberStr != "") {
+                int number = Integer.parseInt(numberStr);
+
+                if (number < 0)
+                    negatives.add(numberStr);
+
+                if (number < 1000)
+                    result += number;
+
+                numberStr = "";
             }
-
-            result += Integer.parseInt(String.valueOf(strings.get(i)));
         }
+
         if (negatives.isEmpty())
             return result;
 
